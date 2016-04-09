5fc0c6ffadceaa37f252e31d5faef91e845ebcf0
smcho <sm.cho@mac.com>
2016-04-08 23:00:18 -0500

    [2016/04/08]11:00PM

--- src/test/scala/util/json/TestJson.scala
+++ src/test/scala/util/json/TestJson.scala
@@ -9,7 +9,7 @@ class TestJson extends FunSuite {
     "age": 10,
     "date": [10, 3, 17],
     "time": [12, 14],
-    "lattitude": [1, 2, 3, 4],
+    "latitude": [1, 2, 3, 4],
     "longitude": [11, 12, 13, 14]
   }"""
 
@@ -19,7 +19,7 @@ class TestJson extends FunSuite {
     assert(m("age") == 10)
     assert(m("date") == Seq(10,3,17))
     assert(m("time") == Seq(12, 14))
-    assert(m("lattitude") == Seq(1,2,3,4))
+    assert(m("latitude") == Seq(1,2,3,4))
     assert(m("longitude") == Seq(11,12,13,14))
   }
 
@@ -29,7 +29,7 @@ class TestJson extends FunSuite {
     assert(m("age") == 10)
     assert(m("date") == Seq(10,3,17))
     assert(m("time") == Seq(12, 14))
-    assert(m("lattitude") == Seq(1,2,3,4))
+    assert(m("latitude") == Seq(1,2,3,4))
     assert(m("longitude") == Seq(11,12,13,14))
   }
 
