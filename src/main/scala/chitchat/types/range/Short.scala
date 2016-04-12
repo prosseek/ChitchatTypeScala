69565d0f1427538769b35098c64936d2e12985d9
smcho <sm.cho@mac.com>
2016-04-11 21:25:07 -0500

    ATN implementation added

---
+++ src/main/scala/chitchat/types/range/Short.scala
@@ -0,0 +1,8 @@
+package chitchat.types.range
+
+/**
+  * Created by smcho on 4/11/16.
+  */
+class Short {
+
+}
---
+++ src/main/scala/chitchat/types/string/Event.scala
@@ -0,0 +1,8 @@
+package chitchat.types.string
+
+/**
+  * Created by smcho on 4/11/16.
+  */
+class Event {
+
+}
